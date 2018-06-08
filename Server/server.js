3c8ad26e87c95006a06da3220d8fa2ffe0219370
eduardo-aop <30639462+eduardo-aop@users.noreply.github.com>
2018-06-08 11:17:55 -0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -37,7 +37,7 @@ app.post('/saveClient', function(req, res) {
 	});
 });
 
-app.get('/doLogin', function(req, res) {
+app.post('/doLogin', function(req, res) {
     console.log(req.body);
 
 	var id = 0;
